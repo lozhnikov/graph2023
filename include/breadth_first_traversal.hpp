5454ac98fd5b770f229ec00cbf5b907c61797648
Mikhail Lozhnikov <lozhnikovma@gmail.com>
2023-04-05 14:29:53 +0300

    Initial commit.

---
+++ include/breadth_first_traversal.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file include/breadth_first_traversal.hpp
+ * @author Mikhail Lozhnikov
+ *
+ * Алгоритм обхода графа в ширину.
+ */
+ 
+#ifndef INCLUDE_BREADTH_FIRST_TRAVERSAL_HPP_
+#define INCLUDE_BREADTH_FIRST_TRAVERSAL_HPP_
+
+namespace graph {
+
+/**
+ * @brief Обход графа в ширину.
+ */
+template<typename GraphType, typename Predicate>
+void BreadthFirstTraversal(GraphType& graph, Predicate predicate) {
+}
+
+}  // namespace graph
+
+#endif  // INCLUDE_BREADTH_FIRST_TRAVERSAL_HPP_
