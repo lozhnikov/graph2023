229d0d886f6caa1a1c8f073edfb1547fe64934f6
Михаил Сёмин <blaziant@DESKTOP-IMSE31O.localdomain>
2023-05-10 15:36:18 +0300

    fix bugs in tests

--- tests/kun_algorithm_test.cpp
+++ tests/kun_algorithm_test.cpp
@@ -1,46 +1,43 @@
-#include <unordered_set>
-#include <vector>
-#include <algorithm>
-#include <utility>
-#include <random>
 #include "test_core.hpp"
+#include <algorithm>
 #include <graph.hpp>
 #include <httplib.h>
-#include <nlohmann/json.hpp>
 #include <kun_algorithm.hpp>
+#include <nlohmann/json.hpp>
+#include <random>
+#include <unordered_set>
+#include <utility>
+#include <vector>
 
-
+using std::make_pair;
+using std::pair;
 using std::unordered_set;
 using std::vector;
-using std::pair;
-using std::make_pair;
 
+using std::mt19937;
 using std::out_of_range;
 using std::random_device;
-using std::mt19937;
 using std::uniform_int_distribution;
 
 using graph::Graph;
 
 void TestKunAlgorithmSimple(httplib::Client *cli) {
-    int num_cases = 1;
-    std::vector<std::pair<nlohmann::json, nlohmann::json>> cases(num_cases);
-    nlohmann::json input = {
-            {"vertices", {1, 2, 3, 4, 5}},
-            {"edges", {{1, 2}, {2, 3}, {3, 4}, {4, 5}}},
-            {"vertices_first_part", {1, 3, 5}},
-            {"vertices_second_part", {2, 4}}
-    };
-    nlohmann::json expected = {
-            {"result", {{2, 1}, {4, 3}}},
-    };
-    
-    auto output = cli->Post("/kun_algorithm", input.dump(), "application/json");
-    REQUIRE(output->body == expected.dump());
-    REQUIRE_EQUAL(output->body, expected.dump());
+  int num_cases = 1;
+  std::vector<std::pair<nlohmann::json, nlohmann::json>> cases(num_cases);
+  nlohmann::json input = {{"vertices", {1, 2, 3, 4, 5}},
+                          {"edges", {{1, 2}, {2, 3}, {3, 4}, {4, 5}}},
+                          {"vertices_first_part", {1, 3, 5}},
+                          {"vertices_second_part", {2, 4}}};
+  nlohmann::json expected = {
+      {"result", {{2, 1}, {4, 3}}},
+  };
+
+  auto output = cli->Post("/kun_algorithm", input.dump(), "application/json");
+  REQUIRE(output->body == expected.dump());
+  REQUIRE_EQUAL(output->body, expected.dump());
 }
 
 void TestKunAlgorithm(httplib::Client *cli) {
-    TestSuite suite("TestKunAlgorithm");
-    RUN_TEST_REMOTE(suite, cli, TestKunAlgorithmSimple);
+  TestSuite suite("TestKunAlgorithm");
+  RUN_TEST_REMOTE(suite, cli, TestKunAlgorithmSimple);
 }
