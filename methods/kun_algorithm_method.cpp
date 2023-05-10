82f2b5173fe5039045355c4461d300cd0acc409c
Михаил Сёмин <blaziant@DESKTOP-IMSE31O.localdomain>
2023-05-10 15:30:24 +0300

    fix bugs in kun_algo_meth

--- methods/kun_algorithm_method.cpp
+++ methods/kun_algorithm_method.cpp
@@ -1,24 +1,27 @@
-#include <nlohmann/json.hpp>
 #include "methods.hpp"
 #include <graph.hpp>
 #include <kun_algorithm.hpp>
-
+#include <nlohmann/json.hpp>
 
 using graph::Graph;
 
-void graph::KunAlgorithmMethod(const nlohmann::json& input, nlohmann::json* output) {
-    Graph graph;
-    for(auto vert: input["vertices"]){
-        graph.AddVertex(vert);
-    }
-    for(auto edge: input["edges"]){
-        graph.AddEdge(edge[0], edge[1]);
-    }
-    std::cout << "KunAlgorithmMethod add result" << "\n";
-    nlohmann::json result = {
-        {"result", KunAlgorithm(graph, input["vertices_first_part"], input["vertices_second_part"])},
-    };
-    (*output) = result;
-    std::cout << "KunAlgorithmMethod DONE" << "\n";
-    return;
+void graph::KunAlgorithmMethod(const nlohmann::json &input,
+                               nlohmann::json *output) {
+  Graph graph;
+  for (auto vert : input["vertices"]) {
+    graph.AddVertex(vert);
+  }
+  for (auto edge : input["edges"]) {
+    graph.AddEdge(edge[0], edge[1]);
+  }
+  std::cout << "KunAlgorithmMethod add result"
+            << "\n";
+  nlohmann::json result = {
+      {"result", KunAlgorithm(graph, input["vertices_first_part"],
+                              input["vertices_second_part"])},
+  };
+  (*output) = result;
+  std::cout << "KunAlgorithmMethod DONE"
+            << "\n";
+  return;
 }
