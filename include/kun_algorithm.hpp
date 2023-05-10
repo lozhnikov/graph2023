04d1b292ca83cd7755b9746b1ab0ef65fe646462
Михаил Сёмин <blaziant@DESKTOP-IMSE31O.localdomain>
2023-05-10 15:18:34 +0300

    fix bugs 2

--- include/kun_algorithm.hpp
+++ include/kun_algorithm.hpp
@@ -1,46 +1,48 @@
 #pragma once
 #include <algorithm>
+#include <graph.hpp>
 #include <iostream>
 #include <vector>
-#include <graph.hpp>
 
 using graph::Graph;
 
-
-bool TryKuhn (size_t v, std::unordered_map<size_t, int>& mt, std::unordered_map<size_t, bool>& used, Graph &graph) {
-	if (used[v]) {
-        return false;
+bool TryKuhn(size_t v, std::unordered_map<size_t, int> &mt,
+             std::unordered_map<size_t, bool> &used, Graph &graph) {
+  if (used[v]) {
+    return false;
+  }
+  used[v] = true;
+  for (size_t to : graph.Edges(v)) {
+    if (mt[to] == -1 || TryKuhn(mt[to], mt, used, graph)) {
+      mt[to] = v;
+      return true;
     }
-	used[v] = true;
-    for (size_t to: graph.Edges(v)) {
-		if (mt[to] == -1 || TryKuhn(mt[to], mt, used, graph)) {
-			mt[to] = v;
-			return true;
-		}
-	}
-	return false;
+  }
+  return false;
 }
 
-std::vector<std::pair<size_t, size_t>> KunAlgorithm(Graph &graph, const std::vector<size_t>& vertices_first_part, const std::vector<size_t>& vertices_second_part) {
-    std::unordered_map<size_t, bool> used;
-    std::unordered_map<size_t, int> mt;
+std::vector<std::pair<size_t, size_t>>
+KunAlgorithm(Graph &graph, const std::vector<size_t> &vertices_first_part,
+             const std::vector<size_t> &vertices_second_part) {
+  std::unordered_map<size_t, bool> used;
+  std::unordered_map<size_t, int> mt;
 
-    for (auto vert: graph.Vertices()) {
-        used[vert] = false;
-        mt[vert] = -1;
-    }
+  for (auto vert : graph.Vertices()) {
+    used[vert] = false;
+    mt[vert] = -1;
+  }
 
-	for (auto v: vertices_first_part) {
-		for (auto vert: graph.Vertices()) {
-            used[vert] = false;
-        }
-		TryKuhn(v, mt, used, graph);
-	}
-    std::vector<std::pair<size_t, size_t>> result;
-    for (auto item : mt) {
-        if(item.second != -1) {
-            result.push_back({item.first, item.second});
-        }
+  for (auto v : vertices_first_part) {
+    for (auto vert : graph.Vertices()) {
+      used[vert] = false;
     }
-    return result;
+    TryKuhn(v, mt, used, graph);
+  }
+  std::vector<std::pair<size_t, size_t>> result;
+  for (auto item : mt) {
+    if (item.second != -1) {
+      result.push_back({item.first, item.second});
+    }
+  }
+  return result;
 }
